782f7c1ec154d6bfad8f2bc84556bd2cf1d9e52b
ABHINAV JAISWAL <mrjaisabhi.14@gmail.com>
2023-02-27 00:37:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontains = document.getElementsByClassName("tab-contains");
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontain of tabcontains) {
+        tabcontain.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
