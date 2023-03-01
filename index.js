e15ce5bcfbea0258f9c5f84b1c9f6b5451e98777
ABHINAV JAISWAL <mrjaisabhi.14@gmail.com>
2023-03-01 15:14:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontains = document.getElementsByClassName("tab-contains");
+ var sidemenu = document.getElementById("sidemenu");
 function opentab(tabname) {
     for (tablink of tablinks) {
         tablink.classList.remove("active-link");
@@ -11,12 +12,11 @@ function opentab(tabname) {
     document.getElementById(tabname).classList.add("active-tab");
 
     
- var sidemenu = document.getElementById("sidemenu");
 
-    function openmenu() {
+ function openmenu() {
         sidemenu.style.right = "0";
     }
-    function closemenu() {
+ function closemenu() {
         sidemenu.style.right = "-200px";
     }
 
