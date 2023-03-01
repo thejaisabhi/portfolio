a0067335dc7a9d052b56e7537811e0cdfce8a779
ABHINAV JAISWAL <mrjaisabhi.14@gmail.com>
2023-03-01 15:07:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,14 @@ function opentab(tabname) {
     }
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
-}
+
+    
+ var sidemenu = document.getElementById("sidemenu");
+
+    function openmenu() {
+        sidemenu.style.right = "0";
+    }
+    function closemenu() {
+        sidemenu.style.right = "-200px";
+    }
 
